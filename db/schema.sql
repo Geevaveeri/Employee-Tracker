5744a37acad5f69ce0d700cad4b424dc648d0c49
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-05-29 13:19:33 -0600

    completed creation of tracker database and departments, roles, and employees tables

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+-- department table
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30)
+);
+
+-- employee roles table
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INTEGER,
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_id)
+        REFERENCES departments(id)
+);
+
+-- employees table
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id),
+    CONSTRAINT fk_manager
+        FOREIGN KEY (manager_id)
+        REFERENCES  employees(role_id)
+        ON DELETE SET NULL
+);
